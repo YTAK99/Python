4572d892d96375199efef11046132705249b13f7
YTAK99 <ytzzang99@gmail.com>
2026-03-24 18:27:18 +0900

    practice

---
+++ python/2.py
@@ -0,0 +1,7 @@
+var1 = int(input())
+var2 = input()
+
+print(var1 * int(var2[2])) 
+print(var1 * int(var2[1])) 
+print(var1 * int(var2[0])) 
+print(var1 * int(var2))
---
+++ python/3.py
@@ -0,0 +1,9 @@
+A = int(input())
+B = int(input())
+
+print('%d + %d = %d'%(A, B, A+B))
+
+
+
+a, b = map(int, input().split())
+print(a+b)
