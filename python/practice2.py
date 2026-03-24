d684873c833fe7f8c2bbec81b7703f55be2153a8
YTAK99 <ytzzang99@gmail.com>
2026-03-24 15:11:32 +0900

    practice

---
+++ python/practice2.py
@@ -0,0 +1,36 @@
+lang = 'PYTHON'
+print(lang[0])
+print(lang[-1])
+print(lang[:3])
+print(lang[2:])
+print(lang[:])
+
+num = 3
+num += 2    #num = num + 2
+print(num)
+
+num -= 1
+num *= 2
+num /= 4
+print(num)
+
+snack = '꿀꽈배기'
+print(len(snack))
+
+snack = '''꿀꽈배기는
+너무
+맛있어요'''
+print(snack)
+
+print('-' * 10)
+print('NadoCoding')
+print('*' * 20)
+
+let = 'hOw aRe yOu?'
+print(let.lower())  #전체 소문자로
+print(let.upper())  #전체 대문자로
+print(let.capitalize())  #전체 중에 첫 문자만 대문자로
+print(let.title())  #각 단어의 첫 문자만 대문자로
+print(let.swapcase())  #대소문자 상호 변환
+print(let.split())  # 문자열 배열로 분리
+print(let.count('O'))   #' ' 안의 문자가 몇번 쓰였는지
