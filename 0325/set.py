7d44c7da396b3e25f35cf3c0113c141c9bdb300f
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 15:21:52 +0900

    f

--- 0325/set.py
+++ 0325/set.py
@@ -1,1 +1,12 @@
+# List : [] 중복O, 순서O, 수정O
+# tuple : {} 중복O, 순서O, 수정X
+# Set : () 중복X, 순서X
 
+# 세트 = {값1, 값2, ...}  순서X, 중복X
+
+A = {"돈가스", "보쌈", "제육덮밥"}
+B = {"짬뽕", "초밥", "제육덮밥"}
+
+print(A.intersection(B))        # A와 B의 교집합
+print(A.union(B))       # A와 B의 합집합
+print(A.difference(B))      # A - B 차집합
