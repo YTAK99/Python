0cceaba91c82cbe73977314908725a8b3981e2d9
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 15:46:25 +0900

    edit
    Refactor input handling and add additional print statements.

--- 0325/input_split.py
+++ 0325/input_split.py
@@ -1,4 +1,17 @@
-a, b = map(int, input(). split())
+a, b = map(int, input().split())
 c = a // b    # 몫
 d = a % b     # 나머지
 print (f'{a} / {b} = {c} ... {d}')
+
+
+'''
+
+a, b = input().split()
+print(a,b)
+
+
+a, b = map(int, input().split())
+print(f'{a} + {b} =', a+b)
+
+
+'''
