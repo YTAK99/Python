c9bd4d75b044f1fe3a4d6bb455f8ba3a00ade801
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 12:30:53 +0900

    Rename
    Demonstrate various list operations in Python.

---
+++ 0325/list.py
@@ -0,0 +1,30 @@
+a = [1, 2, '삼', 'four']
+b = ['BTS', '봉준호', '손흥민', '제이팍', 'Let`s go']
+
+print(a[0])
+print(a[0:2])
+print('삼' in a)
+print(len(a))
+
+a[2] = '석삼'
+print(a)
+
+a.append('다섯')    # 값 추가
+print(a)
+
+a.remove(2)    # 값 삭제
+print(a)
+
+a.extend(b)     # 리스트 확장
+print(a)
+
+# insert()      # 원하는 위치에 값 추가
+# pop()         # 원하는 위치 의 값 삭제
+# clear()       # 모든 값 삭제
+# sort()        # 값 순서대로 정렬렬  
+# reverse()     # 순서 뒤집기
+# copy()        # 리스트 복사
+# count()       # 어떤 값이 몇 개 있는지
+# index()       # 어떤 값이 어디에 있는지
+
+
