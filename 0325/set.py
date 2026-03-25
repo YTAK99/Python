2fda08e0d3aeec7eff8ae70227e6d88810fe7f81
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 15:25:59 +0900

    e

--- 0325/set.py
+++ 0325/set.py
@@ -10,3 +10,24 @@ B = {"짬뽕", "초밥", "제육덮밥"}
 print(A.intersection(B))        # A와 B의 교집합
 print(A.union(B))       # A와 B의 합집합
 print(A.difference(B))      # A - B 차집합
+
+A.add('닭갈비')
+print(A)
+
+A.remove('제육덮밥')
+print(A)
+
+A.clear()
+print(A)        # 모든 값 삭제
+
+del A           # 완전 삭제
+print(A)        # 그래서 없다고 나옴
+
+'''
+copy()      세트복사
+discard()       값삭제(해당값이없어도에러발생X)
+isdisjoint()        두세트에겹치는값이없는지여부
+issubset()      다른세트의부분집합인지여부
+issuperset()        다른세트의상위집합인지여부
+update()        다른세트의값들을더함
+'''
