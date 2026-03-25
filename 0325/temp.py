b26dd7ed8f5e2a83f25826d0b69f8195ccd90384
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 15:42:17 +0900

    temporilary

---
+++ 0325/temp.py
@@ -0,0 +1,14 @@
+'''
+
+i = int(input('정수를 입력하세요: '))
+f = float(input('실수를 입력하세요: '))
+T = i + f
+
+print(f'{i} + {f} = {T}')
+
+
+
+a, b = map(int, input(), split())
+print("5 + 10 =". a+b)
+
+'''
