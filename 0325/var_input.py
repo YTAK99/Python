6556bddeb34bbfd6c2cc4a278c7c1859fdffc255
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-25 16:32:05 +0900

    edit

---
+++ 0325/var_input.py
@@ -0,0 +1,37 @@
+a, b, c = input().split()
+b, c = int(b), float(c)
+
+print('name:', a)
+print('height:', b)
+print('weight:', c)
+
+
+
+
+'''
+
+# 입력 예: 찰스 180 75.5
+data = input().split()
+
+a = data[0]          # 이름 (이미 문자열)
+b = int(data[1])     # 키 (정수)
+c = float(data[2])   # 몸무게 (실수)
+
+print('name:', a)
+print('height:', b)
+print('weight:', c)
+
+
+##################################
+
+
+a1, a2 = input().split()
+b1, b2 = input().split()
+a2, b2 = int(a2), int(b2)
+
+print(f'{a1} was born in {a2}')
+print(f'{b1} was born in {b2}')
+print(f'{a1} is {b2-a2} years older than {b1}')
+
+
+'''
