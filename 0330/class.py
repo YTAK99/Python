3106f41f181394a04e2b1551e0522d78b5cbab81
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-30 17:30:26 +0900

    Update class.py

--- 0330/class.py
+++ 0330/class.py
@@ -96,6 +96,47 @@ class BlackBox:
 
 
 
+def add(num1, num2):             # 전달값
+
+add(1,2):           # 함수 호출
+
+
+class BlackBox:
+    def set_travel_mode(self, min):           # 클래스 메소드(전달값1, 전달값2)
+b1.set_travel_mode(20)           # 메소드 호출
+
+
+###################################################################################
+
+
+class BlackBox:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+    def set_travel_mode(self, min):
+        print(f'{self.name} {min}분 동안 여행 모드 ON')
+
+b1 = BlackBox('까망이', 200000)
+b2 = BlackBox('하양이', 100000)
+b1.set_travel_mode(20) 
+b2.set_travel_mode(10) 
+
+BlackBox.set_travel_mode(b1, 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
