f14ab276f3078301a214b22e4d771646ae8e6b7c
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-03-30 10:20:35 +0900

    Add local variable examples in local_variable.py

---
+++ 0330/local_variable.py
@@ -0,0 +1,25 @@
+def secret():
+    message = '이건 나만의 비밀'
+    print(message)
+
+message2 = '이것도 비밀'
+# print(message)      # 지역변수라 사용안됨
+
+print(message2)
+secret()
+
+def please():
+    message = '이렇게 하면 되지?'
+    print(message)      # 이름은 같지만 다른 변수
+please()
+
+
+
+
+
+
+
+
+
+
+
