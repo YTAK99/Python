85d4c9e59aaa4d7155fc605651fee8c1ee0acd65
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-27 17:09:29 +0900

    Create 좌표_by_Class.py

---
+++ 문제풀이/좌표_by_Class.py
@@ -0,0 +1,25 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Coordinate(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        return Coordinate(self.x - other.x, self.y - other.y)
+    def center(self, other):
+        return Coordinate((self.x + other.x) / 2, (self.y + other.y) / 2)
+    # def center(p1, p2):
+    # return Coordinate((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+    def __str__(self):
+        return f"({self.x:.1f}, {self.y:.1f})"
+
+x1, y1 = map(float, input().split())
+x2, y2 = map(float, input().split())
+
+p1 = Coordinate(x1, y1)
+p2 = Coordinate(x2, y2)
+
+print("add =", p1 + p2)
+print("sub =", p1 - p2)
+print("center =", p1.center(p2))
