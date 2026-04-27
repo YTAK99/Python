4c9379e60edb492f0b5935c140703cc5f476776e
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-27 13:17:03 +0900

    Update 나머지.py

--- 문제풀이/나머지.py
+++ 문제풀이/나머지.py
@@ -1,24 +1,6 @@
 # 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다.
 # 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 
-lst = []
-newlst = []
-count = 0
-
-for _ in range(10):
-    lst.append(int(input()))
-
-for x in lst:
-    newlst.append(x % 42)
-
-for y in newlst:
-    if y not in newlst[:count]:
-        count += 1
-
-print(count)
-
-########################################################################
-
 newlst = []
 unique = []
 
