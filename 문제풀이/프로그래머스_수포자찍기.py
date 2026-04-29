b41046a2cc6fe972afa551e34ff09ad519167694
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-29 18:22:49 +0900

    Update 프로그래머스_수포자찍기.py

--- 문제풀이/프로그래머스_수포자찍기.py
+++ 문제풀이/프로그래머스_수포자찍기.py
@@ -25,3 +25,38 @@ def solution(answers):
             result.append(i + 1)
     
     return result
+
+#########################################################################################################
+
+import itertools
+def solution(answers):
+    count1 = itertools.cycle([1,2,3,4,5])
+    count2 = itertools.cycle([2,1,2,3,2,4,2,5])
+    count3 = itertools.cycle([3,3,1,1,2,2,4,4,5,5])
+
+    Ii=0
+    Jj=0
+    Kk=0
+    
+
+    for a,b in zip(answers,count1):
+        if a == b:
+            Ii+=1
+    for c,d in zip(answers,count2):
+        if c == d:
+            Jj+=1
+    for e,f in zip(answers,count3):
+        if e == f:
+            Kk+=1
+    list1 = [Ii,Jj,Kk]
+    hap = max(list1)
+    result = []
+
+    if list1[0] == hap:
+        result.append(1)
+    if list1[1] == hap:
+        result.append(2)
+    if list1[2] == hap:
+        result.append(3)
+    
+    return result
