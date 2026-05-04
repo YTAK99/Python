c1831b6d729c3ec5b6187051812798702fc4b221
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-05-04 18:26:24 +0900

    Update 별찍기_역순.py

--- 0422/별찍기_역순.py
+++ 0422/별찍기_역순.py
@@ -7,3 +7,17 @@ for x in range(N):
     for y in range(0, N-x):
         print('*', end='')
     print()
+
+########################################################################################
+
+# 자리 맞춰서 출력
+
+N = int(input())
+
+for x in range(N):
+    for y in range(x):
+        print(" ", end="")
+        
+    for z in range(N-x, 0, -1):
+        print("*", end="")
+    print()
