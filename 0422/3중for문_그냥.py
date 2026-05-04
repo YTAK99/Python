fe5df082d876856f3ea57122badcc0c2d9dee8a2
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-05-04 11:51:47 +0900

    Create 3중for문_그냥.py

---
+++ 0422/3중for문_그냥.py
@@ -0,0 +1,6 @@
+for x in range(1, 10):
+    for y in range(1, 10):
+        for z in range(1, 10):
+            print(f'z는 {z}', end=' ')
+        print(f'y는 {y}', end=' ')
+    print(f'x는 {x}', end=' ')
