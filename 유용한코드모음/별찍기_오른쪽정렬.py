6b793fc70af900cabe1570a83a50bdd887fba4bb
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-05-04 11:59:33 +0900

    Update 별찍기_오른쪽정렬.py

--- 유용한코드모음/별찍기_오른쪽정렬.py
+++ 유용한코드모음/별찍기_오른쪽정렬.py
@@ -22,3 +22,10 @@ for i in range(1, 4):
 
 for i in range(1, 4):
     print('%3s' % ('*' * i))
+
+########################################################
+
+N = int(input())
+
+for i in range(N, 0, -1):
+    print(' '*(N-i) + '*'*i)
