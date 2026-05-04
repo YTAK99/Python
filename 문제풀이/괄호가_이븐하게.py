555884cf001178439a84d10012ffc750c3196ade
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-05-04 11:54:00 +0900

    Update 괄호가_이븐하게.py

--- 문제풀이/괄호가_이븐하게.py
+++ 문제풀이/괄호가_이븐하게.py
@@ -67,3 +67,36 @@ N = int(input())
 
 for _ in range(N):
     check()
+
+#################################################################################
+
+# total = 0
+
+# (가 나오면 total+=1 
+# )가 나오면 total-=1
+
+# 근데 중간에 한번이라도 total < 0 되면 탈락
+
+T = int(input())
+
+for _ in range(T):
+    total = 0
+    vps = input()
+    boole = True
+    
+    for x in vps:
+        if x == '(':
+            total += 1
+        elif x == ')':
+            total -= 1
+
+        if total < 0:
+            print("NO")
+            boole = False
+            break
+    
+    if boole:        
+        if total == 0:
+            print("YES")
+        else:
+            print("NO")
